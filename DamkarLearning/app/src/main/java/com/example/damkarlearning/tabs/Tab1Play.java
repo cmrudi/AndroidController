8f56d821ef220b198165204c33b60c81d43f3bd6
Faza Thirafi <faza.thirafi@gmail.com>
2017-04-16 16:59:43 +0700

    landscape layout

--- DamkarLearning/app/src/main/java/com/example/damkarlearning/tabs/Tab1Play.java
+++ DamkarLearning/app/src/main/java/com/example/damkarlearning/tabs/Tab1Play.java
@@ -2,6 +2,7 @@ package com.example.damkarlearning.tabs;
 
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,6 +14,8 @@ import com.example.damkarlearning.R;
 import com.example.damkarlearning.SensorControllerActivity;
 import com.example.damkarlearning.TouchControllerActivity;
 
+import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
+
 public class Tab1Play extends Fragment implements View.OnClickListener {
     Button sButton;
     Button tButton;
@@ -21,8 +24,13 @@ public class Tab1Play extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        view = inflater.inflate(R.layout.tab1play, container, false);
+        int screenOrientation = getResources().getConfiguration().orientation;
+        if (screenOrientation == ORIENTATION_LANDSCAPE) {
+            view = inflater.inflate(R.layout.tab1play_landscape, container, false);
+        }
+        else {
+            view = inflater.inflate(R.layout.tab1play, container, false);
+        }
         sButton = (Button) view.findViewById(R.id.sensorButton);
         sButton.setOnClickListener(this);
         tButton = (Button) view.findViewById(R.id.touchButton);
@@ -49,5 +57,17 @@ public class Tab1Play extends Fragment implements View.OnClickListener {
                 break;
         }
 
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            getActivity().setContentView(R.layout.tab1play_landscape);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            getActivity().setContentView(R.layout.tab1play);
+        }
     }
 }
